d59c07eefb493fc12bbe4b6c53f4be172a766f85
Talha <105781221+mtTalha@users.noreply.github.com>
2022-05-19 13:47:15 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+alert("hello world")
+alert("My name is Talha Haseeb")
+alert("My father name is Abdull Haseeb")
+alert("I am 18 year old ")
+alert("Certified Mobile Application Development")
+alert("PIZZA")
+alert("talhahaseeb@gmail.com")
+alert("A smarter way to learn JavaScript.")
+
+
+
+
